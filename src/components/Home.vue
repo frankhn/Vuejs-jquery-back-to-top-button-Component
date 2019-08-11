ec5ee1cccfafac399b52b6c3ceecfb57e3e7f8f1
frankhn <harfrank2@gmail.com>
2019-08-11 19:12:01 +0200

    back to top vue component example

---
+++ src/components/Home.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="hello">
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+           <button class="goTop" v-if="isVisible" @click="backToTop">
+        <i class="fa fa-angle-up" aria-hidden="true"></i>
+    </button>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+     <button class="goTop" v-if="isVisible" @click="backToTop">
+        <i class="fa fa-angle-up" aria-hidden="true"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+    template: '#backtotop',
+  data: function() {
+    return {
+      isVisible: false
+    };
+  },
+  methods: {
+    initToTopButton: function() {
+      $(document).bind('scroll', function() {
+        var backToTopButton = $('.goTop');
+        if ($(document).scrollTop() > 350) {
+          backToTopButton.addClass('isVisible');
+          this.isVisible = true;
+        } else {
+          backToTopButton.removeClass('isVisible');
+          this.isVisible = false;
+        }
+      }.bind(this));
+    },
+    backToTop: function() {
+      $('html,body').stop().animate({
+        scrollTop: 0
+      }, 'slow', 'swing');
+    }
+  },
+  mounted: function() {
+    this.$nextTick(function() {
+      this.initToTopButton();
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  height: 30em;
+  margin: 0 10px;
+}
+li:nth-child(odd) {
+  background: #dddd;
+}
+a {
+  color: #42b983;
+}
+
+
+.goTop {
+  border-radius: 5px;
+  color: #42b983;
+  background-color: rgb(1,14,27);
+  background-color: rgba(1, 14, 27, .7);
+  position: fixed;
+  width: 45px;
+  height: 45px;
+  display: block;
+  right: 15px;
+  bottom: 15px;
+  border: none;
+  opacity: 0;
+  z-index: -1;
+  .fa {
+    color: white;
+    font-size: 22px;
+  }
+  &:hover {
+    opacity: 1;
+    background-color: rgb(1,14,27);
+    background-color: rgba(1, 14, 27, .9);
+  }
+}
+
+.isVisible {
+  opacity: .8;
+  z-index: 4;
+  transition: all .4s ease-in;
+}
+</style>
